c167a9372a6125617972044bb15b067b8e488161
GD-0606 <mgopi1171533@gmail.com>
2025-06-02 23:24:50 +0530

    changes in format

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-
 import './App.css';
 import TodoList from './component/todolist';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
-import { BrowserRouter , Link, Route,Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Home from './component/home';
 function App() {
-  return (<>
-  <TodoList/>
-   </> );
+  return (
+    <>
+      <TodoList />
+    </>
+  );
 }
 
 export default App;
